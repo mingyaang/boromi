d985279ce25c09b8a9074024e2331740e6151216
mingyaang <mmao@ualberta.ca>
2020-10-23 17:16:14 -0600

    Added Dagger for DI, AuthController logic, and sample execution code

--- app/src/main/java/com/team41/boromi/MainActivity.java
+++ app/src/main/java/com/team41/boromi/MainActivity.java
@@ -1,14 +1,19 @@
 package com.team41.boromi;
 
+import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+import javax.inject.Singleton;
 
+@Singleton
 public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    Intent intent = new Intent(this, TestActivity.class);
+    startActivity(intent);
   }
 }
